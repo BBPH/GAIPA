43b69ac8304ef419b1fb14f31bd239aaf43502da
BBPH <116148344+BBPH@users.noreply.github.com>
2025-11-23 14:21:48 +0000

    delete two.

--- prog1.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import openai
-import base64
-from openai import OpenAI
-
-st.title(":blue[Assignment!]")
-api_key = st.text_input(":blue[Api key!!]", type="password")
-client = OpenAI(api_key=api_key)
-
